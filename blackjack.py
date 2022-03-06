318e23c14096aff00023eea3ef8e715d188a3e2b
Artur Ciepielowski <artciepi@gmail.com>
2022-03-06 13:14:13 +0100

    Added new functions

--- blackjack.py
+++ blackjack.py
@@ -3,20 +3,28 @@ from tkinter import Y
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+myScore = 0
+compScore = 0
 my_cards = []
 comp_cards = []
 
-my_cards.append(cards[random.randint(0, 12)])
-my_cards.append(cards[random.randint(0, 12)])
-myScore = 0
-for x in my_cards:
-    myScore = myScore + x
 
-comp_cards.append(cards[random.randint(0, 12)])
-comp_cards.append(cards[random.randint(0, 12)])
-compScore = 0
-for y in comp_cards:
-    compScore = compScore + y
+def cardShuffle():
+
+    my_cards = []
+    comp_cards = []
+
+    my_cards.append(cards[random.randint(0, 12)])
+    my_cards.append(cards[random.randint(0, 12)])
+    myScore = 0
+    for x in my_cards:
+        myScore = myScore + x
+
+    comp_cards.append(cards[random.randint(0, 12)])
+    comp_cards.append(cards[random.randint(0, 12)])
+    compScore = 0
+    for y in comp_cards:
+        compScore = compScore + y
 
 
 def firstWinCondition():
@@ -65,6 +73,18 @@ def winCondition():
         blackJack = False
 
 
+def looseCondition():
+    if myScore > 21:
+        print(
+            f"The Player lost. \n Computer score: {compScore} \n Player score: {myScore}"
+        )
+    elif compScore > 21:
+        print(
+            f"The Computer lost. \n Computer score: {compScore} \n Player score: {myScore}"
+        )
+
+
+cardShuffle()
 blackJack = True
 while blackJack:
 
@@ -90,8 +110,11 @@ while blackJack:
                         myScore = myScore + x
                     print(f"Your cards :{my_cards},   current score: {myScore}")
                     print(f"Computer first card : {comp_cards[0]}")
+                    looseCondition()
                 elif gameContinue == "n":
                     loop = False
-                winCondition()
+                    winCondition()
+                    my_cards = []
+                    comp_cards = []
     elif Game == "n":
         blackJack = False
